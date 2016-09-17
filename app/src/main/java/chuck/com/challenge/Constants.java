8e10ef2e979acf9367745dfd579e41e00e909995
Laurence43210 <laurence.smithdixon@gmail.com>
2016-09-17 14:50:58 +0100

    Constants file

---
+++ app/src/main/java/chuck/com/challenge/Constants.java
@@ -0,0 +1,15 @@
+package chuck.com.challenge;
+
+/**
+ * Created by Laurence on 17/09/2016.
+ */
+public class Constants {
+
+    public static String API_BASE_LINK = "http://api.icndb.com/jokes/random";
+
+    public static int SINGLE_JOKE_QUANTITY = 1;
+
+    public static int BATCH_JOKE_QUANTITY = 20;
+
+
+}
