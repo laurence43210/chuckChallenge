88607015a52689d8a55dec78f0ab3d138264720f
Laurence43210 <laurence.smithdixon@gmail.com>
2016-09-17 14:39:30 +0100

    Integrated Volley

--- app/src/main/java/chuck/com/challenge/ChuckChallengeApplication.java
+++ app/src/main/java/chuck/com/challenge/ChuckChallengeApplication.java
@@ -1,7 +1,9 @@
 package chuck.com.challenge;
 
-import android.app.Application;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
 
+import android.app.Application;
 
 /**
  * Created by Laurence on 17/09/2016.
@@ -10,14 +12,21 @@ public class ChuckChallengeApplication extends Application {
 
     static ChuckChallengeApplication sInstance;
 
+    static RequestQueue sRequestQueue;
+
     @Override
     public void onCreate() {
         super.onCreate();
         sInstance = this;
+        sRequestQueue = Volley.newRequestQueue(sInstance);
     }
 
     public static ChuckChallengeApplication getInstance() {
         return sInstance;
+    }
+
+    public RequestQueue getVolleyRequestQueue() {
+        return sRequestQueue;
     }
 
 }
---
+++ app/src/main/java/chuck/com/challenge/Enums/ServerCallEnum.java
@@ -0,0 +1,9 @@
+package chuck.com.challenge.Enums;
+
+/**
+ * Created by Laurence on 17/09/2016.
+ */
+public enum ServerCallEnum {
+    RANDOM,
+    NAME_REPLACE;
+}
