e3a3fcaa72d9fc4dc476c55ebb7febf67c99cbd7
Laurence43210 <laurence.smithdixon@gmail.com>
2016-09-17 18:42:00 +0100

    added onStop to name Replace Activity

--- app/src/main/java/chuck/com/challenge/activities/nameReplaceActivity/nameReplaceActivity.java
+++ app/src/main/java/chuck/com/challenge/activities/nameReplaceActivity/nameReplaceActivity.java
@@ -1,14 +1,12 @@
 package chuck.com.challenge.activities.nameReplaceActivity;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
 
+
+import chuck.com.challenge.Enums.ServerCallEnum;
 import chuck.com.challenge.R;
 import chuck.com.challenge.activities.baseActivity.BaseActivity;
+import chuck.com.challenge.helpers.VolleyHelper;
 
 public class NameReplaceActivity extends BaseActivity {
 
@@ -18,4 +16,9 @@ public class NameReplaceActivity extends BaseActivity {
         setContentView(R.layout.activity_name_replace);
     }
 
+    @Override
+    protected void onStop() {
+        super.onStop();
+        VolleyHelper.cancelAllRequests(ServerCallEnum.RANDOM.toString());
+    }
 }
