7c7812d723bb36ba682d45cc7a7d5902e2d7c51e
Laurence43210 <laurence.smithdixon@gmail.com>
2016-09-17 13:14:56 +0100

    Added a launch activity and fragment, and refactored the base activity to display child activity in layout

--- app/src/main/java/chuck/com/challenge/activities/baseActivity/BaseActivity.java
+++ app/src/main/java/chuck/com/challenge/activities/baseActivity/BaseActivity.java
@@ -1,19 +1,34 @@
 package chuck.com.challenge.activities.baseActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.support.annotation.LayoutRes;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.widget.FrameLayout;
 
 import chuck.com.challenge.R;
 
 public class BaseActivity extends AppCompatActivity implements GlobalListener {
 
+    private FrameLayout frameLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_base);
+        super.setContentView(R.layout.activity_base);
+        frameLayout = (FrameLayout) findViewById(R.id.activityContent);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
     }
+
+    @Override
+    public void setContentView(@LayoutRes int layoutResID) {
+        LayoutInflater inflater = (LayoutInflater) this
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        inflater.inflate(layoutResID, frameLayout);
+    }
+
 
 }
---
+++ app/src/main/java/chuck/com/challenge/activities/launchActivity/LaunchFragment.java
@@ -0,0 +1,25 @@
+package chuck.com.challenge.activities.launchActivity;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import chuck.com.challenge.R;
+import chuck.com.challenge.activities.baseActivity.BaseFragment;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class LaunchFragment extends BaseFragment {
+
+    public LaunchFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_launch, container, false);
+    }
+}
