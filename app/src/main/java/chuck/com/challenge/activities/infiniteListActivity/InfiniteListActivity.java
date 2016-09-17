c134456794d1d4f3dfae6c866d30af04b399794c
Laurence43210 <laurence.smithdixon@gmail.com>
2016-09-17 18:36:33 +0100

    infinite list fragment recycleview added

--- app/src/main/java/chuck/com/challenge/activities/infiniteListActivity/InfiniteListActivity.java
+++ app/src/main/java/chuck/com/challenge/activities/infiniteListActivity/InfiniteListActivity.java
@@ -1,14 +1,11 @@
 package chuck.com.challenge.activities.infiniteListActivity;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
 
 import chuck.com.challenge.R;
+import chuck.com.challenge.Enums.ServerCallEnum;
 import chuck.com.challenge.activities.baseActivity.BaseActivity;
+import chuck.com.challenge.helpers.VolleyHelper;
 
 public class InfiniteListActivity extends BaseActivity {
 
@@ -16,6 +13,12 @@ public class InfiniteListActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_infinite_list);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        VolleyHelper.cancelAllRequests(ServerCallEnum.NAME_REPLACE.toString());
     }
 
 }
--- app/src/main/java/chuck/com/challenge/activities/infiniteListActivity/InfiniteListFragment.java
+++ app/src/main/java/chuck/com/challenge/activities/infiniteListActivity/InfiniteListFragment.java
@@ -1,10 +1,13 @@
 package chuck.com.challenge.activities.infiniteListActivity;
 
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import chuck.com.challenge.R;
 import chuck.com.challenge.activities.baseActivity.BaseFragment;
 
@@ -13,13 +16,23 @@ import chuck.com.challenge.activities.baseActivity.BaseFragment;
  */
 public class InfiniteListFragment extends BaseFragment {
 
+    @BindView(R.id.recyclerView)
+    RecyclerView recyclerView;
+
     public InfiniteListFragment() {
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_infinite_list, container,
-                false);
+        View view = inflater.inflate(R.layout.fragment_infinite_list,
+                container, false);
+        ButterKnife.bind(this, view);
+        initUI();
+        return view;
+    }
+
+    private void initUI() {
+        
     }
 }
