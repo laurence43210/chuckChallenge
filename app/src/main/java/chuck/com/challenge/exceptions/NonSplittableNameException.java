1179f83959defacc07bbe486ada7598cd4e9844e
Laurence43210 <laurence.smithdixon@gmail.com>
2016-09-19 23:03:08 +0100

    javadocs for exception

--- app/src/main/java/chuck/com/challenge/exceptions/NonSplittableNameException.java
+++ app/src/main/java/chuck/com/challenge/exceptions/NonSplittableNameException.java
@@ -2,11 +2,17 @@ package chuck.com.challenge.exceptions;
 
 /**
  * Created by Laurence on 19/09/2016.
+ *
+ * An Exception to be thrown when a name is invalid and cannot be split,
+ * this does not contain sufficient groups to be processed for the service.
+ *
+ * very unlikely to be thrown, but acts as a failsafe.
  */
+
+
 public class NonSplittableNameException extends Exception {
 
-    //An Exception to be thrown when a name is invalid and cannot be split, this does not contain sufficient groups to be processed for the service.
-
-    //very unlikely to be thrown, but acts as a failsafe.
-
+    public NonSplittableNameException() {
+        super();
+    }
 }
