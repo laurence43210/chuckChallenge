713e83ea8d4df37223cc252c8e9f18e935def10c
laurence <laurence.smithdixon@gmail.com>
2019-05-22 19:52:41 +0100

    migrate to android x

--- app/src/main/java/chuck/com/challenge/activities/baseActivity/BaseFragment.java
+++ app/src/main/java/chuck/com/challenge/activities/baseActivity/BaseFragment.java
@@ -2,8 +2,8 @@ package chuck.com.challenge.activities.baseActivity;
 
 import android.content.Context;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
 import android.view.View;
 
 import chuck.com.challenge.appListeners.GlobalListener;
