adc9d16ef1667a0a1cd28dadaef05d1aa74c77b3
laurence <laurence.smithdixon@gmail.com>
2019-05-23 16:36:26 +0100

    added a repository to use the new RX calls

--- app/src/main/java/chuck/com/challenge/di/modules/RepositoryModule.kt
+++ app/src/main/java/chuck/com/challenge/di/modules/RepositoryModule.kt
@@ -1,14 +1,24 @@
 package chuck.com.challenge.di.modules
 
-import chuck.com.challenge.Models.AsyncJokeRetriever
-import chuck.com.challenge.appListeners.ChuckNorrisAPI
+import chuck.com.challenge.data.remote.JokesRemoteDataSource
+import chuck.com.challenge.data.repositories.AsyncJokeRetriever
+import chuck.com.challenge.data.remote.network.ChuckNorrisAPI
+import chuck.com.challenge.data.repositories.JokesRepository
 import chuck.com.challenge.helpers.SharedPreferencesHelper
 import dagger.Module
 import dagger.Provides
+import io.reactivex.Scheduler
+import javax.inject.Named
 
 @Module
 class RepositoryModule {
 
     @Provides
-    fun jokeRepository(chuckNorrisAPI: ChuckNorrisAPI, sharedPreferencesHelper: SharedPreferencesHelper) = AsyncJokeRetriever(chuckNorrisAPI, sharedPreferencesHelper)
+    fun provideAsyncJokeRetriever(chuckNorrisAPI: ChuckNorrisAPI, sharedPreferencesHelper: SharedPreferencesHelper) = AsyncJokeRetriever(chuckNorrisAPI, sharedPreferencesHelper)
+
+    @Provides
+    fun provideJokeRepository(remoteDataSource: JokesRemoteDataSource, sharedPreferencesHelper: SharedPreferencesHelper) = JokesRepository(remoteDataSource, sharedPreferencesHelper)
+
+    @Provides
+    fun provideJokesRemoteDataSource(chuckNorrisAPI: ChuckNorrisAPI, @Named(NAME_IO_SCHEDULER) ioScheduler: Scheduler, @Named(NAME_ANDROID_SCHEDULER_MAIN_THREAD) uiScheduler: Scheduler) = JokesRemoteDataSource(chuckNorrisAPI, ioScheduler, uiScheduler)
 }
